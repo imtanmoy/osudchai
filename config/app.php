5ffd540c9dff8b259407f539868702074da9bc9c
Tanmoy Banik <imtanmoybanik@gmail.com>
2018-07-06 15:05:52 +0600

    rules and permission package added and config set

--- config/app.php
+++ config/app.php
@@ -151,6 +151,7 @@ return [
          * Package Service Providers...
          */
         Collective\Html\HtmlServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Application Service Providers...
