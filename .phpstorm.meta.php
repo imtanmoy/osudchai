658d983a9dcd696d591d8093c70d13bdb5c698d8
Tanmoy Banik <imtanmoybanik@gmail.com>
2018-07-10 09:57:16 +0600

    auth register done with mobile

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-07-09 03:46:20.
+    * Generated on 2018-07-10 03:47:49.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.6.26 on 2018-07-09 03:46:19.
+ * Generated for Laravel 5.6.26 on 2018-07-10 03:47:49.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
