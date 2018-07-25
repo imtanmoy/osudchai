3f90f68d8470c862d3ba4c114d108ee0363a08e9
Tanmoy Banik <imtanmoybanik@gmail.com>
2018-07-25 19:18:44 +0600

    dsda

--- app/Http/Controllers/Auth/AccountKitController.php
+++ app/Http/Controllers/Auth/AccountKitController.php
@@ -10,6 +10,8 @@ use GuzzleHttp\Exception\RequestException;
 
 class AccountKitController extends Controller
 {
+
+    protected $client;
     /**
      * $appId
      * @var [int]
@@ -56,22 +58,60 @@ class AccountKitController extends Controller
     public function login(Request $request)
     {
 //        dd($request, $this->appId);
-        $url = $this->tokenExchangeUrl . 'grant_type=authorization_code' .
-            '&code=' . $request->get('code') .
-            "&access_token=AA|.$this->appId.|.$this->appSecret";
-
+//        $url = $this->tokenExchangeUrl . 'grant_type=authorization_code' .
+//            '&code=' . $request->get('code') .
+//            "&access_token=AA|.$this->appId.|.$this->appSecret";
+//
 //        dd($url);
-        try {
-            $apiRequest = $this->client->request('GET', $url);
-            $body = json_decode($apiRequest->getBody());
-            $this->userAccessToken = $body->access_token;
-            $this->refreshInterval = $body->token_refresh_interval_sec;
-            return $this->getData();
-//            dd($body);
-        } catch (GuzzleException $e) {
-            dd($e);
-        }
+////        try {
+////            $apiRequest = $this->client->request('GET', $url);
+////            $body = json_decode($apiRequest->getBody());
+////            $this->userAccessToken = $body->access_token;
+////            $this->refreshInterval = $body->token_refresh_interval_sec;
+////            return $this->getData();
+//////            dd($body);
+////        } catch (GuzzleException $e) {
+////            dd($e);
+////        }
+///
+///
+///
+        $app_id = '206569199896807';
+        $secret = '1b621efd74d110deef47cce1c8acf77';
+        $version = 'v1.1'; // 'v1.1' for example
+
+        $token_exchange_url = 'https://graph.accountkit.com/' . $version . '/access_token?' .
+            'grant_type=authorization_code' .
+            '&code=' . $request->get('code') .
+            "&access_token=AA|$app_id|$secret";
+
+        $data = $this->doCurl($token_exchange_url);
+
+        print_r($data) ;
+//        $user_id = $data['id'];
+//        $user_access_token = $data['access_token'];
+//        $refresh_interval = $data['token_refresh_interval_sec'];
+//
+//        $me_endpoint_url = 'https://graph.accountkit.com/' . $version . '/me?' .
+//            'access_token=' . $user_access_token;
+//        $data = $this->doCurl($me_endpoint_url);
+//        $phone = isset($data['phone']) ? $data['phone']['number'] : '';
+//        $email = isset($data['email']) ? $data['email']['address'] : '';
+//
+//        dd($user_access_token, $phone, $email, $user_id, $refresh_interval);
     }
+
+
+    function doCurl($url)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $data = json_decode(curl_exec($ch), true);
+        curl_close($ch);
+        return $data;
+    }
+
 
     /**
      * @throws GuzzleException
